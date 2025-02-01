ccc22967c66e4e4410bbbf00b46c46cfb069c56d
Pierre <pierre.bouffard-vercelli@epc-groupe.com>
2025-02-01 12:11:56 +0100

    add fallback route and home component

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,9 +1,11 @@
 import { Routes } from '@angular/router';
 import { MapDisplayComponent } from './map-display/map-display.component';
 import { AppComponent } from './app.component';
+import { HomePageComponent } from './home-page/home-page.component';
 
 
 export const routes: Routes = [
-    { path: '', component: AppComponent },
-    { path: 'map-display', component: MapDisplayComponent }
+    { path: '', component: HomePageComponent },
+    { path: 'map-display', component: MapDisplayComponent },
+    { path: '**', redirectTo: '', pathMatch: 'full' } // Fallback route
 ];
