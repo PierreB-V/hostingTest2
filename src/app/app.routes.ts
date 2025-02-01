d48a1fab2ffaa8b83c2593015132534d2c45ea3e
Pierre <pierre.bouffard-vercelli@epc-groupe.com>
2025-02-01 11:35:43 +0100

    add new map component

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,9 @@
 import { Routes } from '@angular/router';
+import { MapDisplayComponent } from './map-display/map-display.component';
+import { AppComponent } from './app.component';
 
-export const routes: Routes = [];
+
+export const routes: Routes = [
+    { path: '', component: AppComponent },
+    { path: 'map-display', component: MapDisplayComponent }
+];
---
+++ src/app/map-display/map-display.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-map-display',
+  imports: [],
+  templateUrl: './map-display.component.html',
+  styleUrl: './map-display.component.scss'
+})
+export class MapDisplayComponent {
+
+}
