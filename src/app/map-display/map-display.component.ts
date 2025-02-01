9f5981d2cc605a2ff7052576392685aa02ac05a1
Pierre <pierre.bouffard-vercelli@epc-groupe.com>
2025-02-01 12:25:15 +0100

    add openlayers basic map

--- src/app/map-display/map-display.component.ts
+++ src/app/map-display/map-display.component.ts
@@ -1,5 +1,9 @@
 import { Component } from '@angular/core';
 import { Router } from '@angular/router';
+import Map from 'ol/Map.js';
+import OSM from 'ol/source/OSM.js';
+import TileLayer from 'ol/layer/Tile.js';
+import View from 'ol/View.js';
 
 @Component({
   selector: 'app-map-display',
@@ -13,5 +17,21 @@ export class MapDisplayComponent {
 
   navigateToHome(): void {
     this.router.navigate(['/']);
+  }
+  ngOnInit(): void {
+    console.log('MapDisplayComponent initialized');
+
+    const map = new Map({
+      target: 'map',
+      layers: [
+        new TileLayer({
+          source: new OSM(),
+        }),
+      ],
+      view: new View({
+        center: [0, 0],
+        zoom: 2,
+      }),
+    });
   }
 }
